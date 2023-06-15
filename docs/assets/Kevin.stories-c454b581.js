a4a3d002a7b8589f83f83f7680ad07817f389ed7
kevin <kevindiegodasilvasousa@gmail.com>
2023-06-15 10:58:46 -0300

    fix: remove docs

--- docs/assets/Kevin.stories-c454b581.js
+++
@@ -1,9 +0,0 @@
-import{a as r,F as m,j as e}from"./jsx-runtime-91a467a5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const u={styleMyProfile:"myProfile"};function n({size:a,circle:l,qrCode:c,sizeQrCode:o}){return r(m,{children:[e("h2",{id:"MyName",children:"Kevin Sousa"}),r("div",{className:u.styleMyProfile,children:[r("div",{children:[e("img",{src:"https://github.com/kevinDsousa.png",alt:"Imagem",width:a,height:a,style:l?{border:"5px solid #262626",borderRadius:"50%"}:{}}),r("ul",{children:[e("li",{children:"Aluno Digital College"}),e("li",{children:"Analista de Redes FTTH"}),e("li",{children:"Transição de carreira"})]})]}),r("div",{children:[e("img",{src:c,alt:"QrCode",width:o,height:o}),e("p",{children:"Escaneie o QRCode para ganhar um amigo"})]})]})]})}try{n.displayName="Kevin",n.__docgenInfo={description:"",displayName:"Kevin",props:{size:{defaultValue:null,description:"Tamanho da minha imagem",name:"size",required:!0,type:{name:"number"}},circle:{defaultValue:null,description:"Adiciona borda arredondada",name:"circle",required:!1,type:{name:"string"}},qrCode:{defaultValue:null,description:"Caminho do QRCode",name:"qrCode",required:!0,type:{name:"string"}},sizeQrCode:{defaultValue:null,description:"Tamanho do QRCode",name:"sizeQrCode",required:!0,type:{name:"number"}}}}}catch{}const p=""+new URL("kevincode-ee7a2bae.png",import.meta.url).href,y={title:"ReactCeará/Kevin",component:n,tags:["autodocs"],args:{circle:!0}},i={args:{size:200,circle:!0,qrCode:p,sizeQrCode:200}};var d,t,s;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
-  args: {
-    size: 200,
-    circle: true,
-    qrCode: kevincode,
-    sizeQrCode: 200
-  }
-}`,...(s=(t=i.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const C=["Kevin_ex01"];export{i as Kevin_ex01,C as __namedExportsOrder,y as default};
-//# sourceMappingURL=Kevin.stories-c454b581.js.map
--- docs/assets/WithTooltip-FBT32F6Q-5ec01e51.js
+++
@@ -1,2 +0,0 @@
-import{W as l,W as s,a as f}from"./index-e0f93d37.js";import"./iframe-51cb5f9a.js";import"../sb-preview/runtime.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./index-cfed0743.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{l as WithToolTipState,s as WithTooltip,f as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-FBT32F6Q-5ec01e51.js.map
